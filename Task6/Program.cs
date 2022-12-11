c719ec9d48493469431c61b220f86d39a1d4d511
orangenaste <orangenaste@gmail.com>
2022-12-11 23:38:23 +0300

    Add task 6

---
+++ Task6/Program.cs
@@ -0,0 +1,14 @@
+﻿//  Напишите программу, которая на вход принимает
+// число и выдает, является ли число четным (делится ли оно на два без остатка).
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
+
+Console.WriteLine("Enter number : ");
+string enterNum = Console.ReadLine();
+int num = Convert.ToInt32(enterNum);
+
+if(num % 2 == 0)
+Console.WriteLine("Even number");
+else
+Console.WriteLine("Not even number");
