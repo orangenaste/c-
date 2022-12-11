d3779ea38f6d68c621d43c95e88eb6a6a172a4c5
orangenaste <orangenaste@gmail.com>
2022-12-11 23:38:41 +0300

    Add task 8

---
+++ Task8/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая на вход принимает
+// число (N), а на выходе показывает все чётные числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+Console.WriteLine("Enter N : ");
+string enterN = Console.ReadLine();
+int N = Convert.ToInt32(enterN);
+
+for (int i = 1; i <= N; i++)
+if (i % 2 == 0)
+Console.WriteLine(i);
