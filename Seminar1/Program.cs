c52d30d8d91bbb2ee262bfe58056e69984ca23a4
orangenaste <orangenaste@gmail.com>
2022-12-11 23:39:29 +0300

    Add test task from seminar

---
+++ Seminar1/Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите программу, которая на вход принимает число и выдает его квадрат (число умноженное 
+// на само себя).
+
+Console.WriteLine("Enter number : ");
+string enterNum = Console.ReadLine();
+int num = Convert.ToInt32(enterNum);
+
+int sqr = num * num;
+
+Console.WriteLine($"Sqr = {sqr} ");
