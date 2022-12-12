d3e4cffedb2577f7bd770eb6702daaa8c1443807
orangenaste <orangenaste@gmail.com>
2022-12-12 17:45:06 +0300

    Add Task 10

---
+++ Task10/Program.cs
@@ -0,0 +1,10 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число 
+// и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.WriteLine("Enter number : ");
+string enterNum = Console.ReadLine();
+int num = Convert.ToInt32(enterNum);
+
