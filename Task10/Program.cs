34996da88c85632b8276c60f9062ad64a86660d2
orangenaste <orangenaste@gmail.com>
2022-12-14 22:43:08 +0300

    Add Task 10

--- Task10/Program.cs
+++ Task10/Program.cs
@@ -4,7 +4,11 @@
 // 782 -> 8
 // 918 -> 1
 
-Console.WriteLine("Enter number : ");
-string enterNum = Console.ReadLine();
-int num = Convert.ToInt32(enterNum);
+var random = new Random();
+int randomNum = random.Next(100, 1000);
+Console.WriteLine(randomNum);
 
+int num1 = randomNum / 100;
+int num2 = randomNum % 100;
+int num3 = num2 / 10;
+Console.WriteLine(num3);
