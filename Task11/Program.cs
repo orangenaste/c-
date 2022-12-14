cbe8b10e183669b5d6fc3c7787548e17c1180a3a
orangenaste <orangenaste@gmail.com>
2022-12-14 22:44:15 +0300

    Add Task11 from seminar

---
+++ Task11/Program.cs
@@ -0,0 +1,28 @@
+﻿// Напишите программу, которая выводит случайное
+// трехзначное число и удаляет вторую цифру этого числа.
+// 456 -> 46
+// 782 -> 72
+// 918 -> 98 
+
+var random = new Random();
+
+int randomNum = random.Next(100, 1000);
+
+Console.Write(randomNum);
+
+int num1 = randomNum / 100;
+int num2 = randomNum / 10;
+int num3 = randomNum - num1*100 - num2*10;
+
+if (num1>num2)
+{
+    Console.WriteLine(num1);
+}
+else if (num1<num2)
+{
+    Console.WriteLine(num2);
+}
+else
+{
+    Console.WriteLine("They are equal");
+}
