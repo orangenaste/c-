50fd9e734211f488993ddf5c2f3dbc56fe879ce8
orangenaste <orangenaste@gmail.com>
2022-12-15 11:34:16 +0300

    Add solution of Task15

--- Task15/Program.cs
+++ Task15/Program.cs
@@ -4,3 +4,18 @@
 // 7 -> да
 // 1 -> нет
 
+Console.Write("Enter number from 1 to 7: "); 
+int num = int.Parse(Console.ReadLine()); 
+if (num >= 1 && num <= 4) 
+{ 
+Console.WriteLine("It's a weekday"); 
+} 
+else if (num >= 6 && num <= 7) 
+{ 
+Console.WriteLine("It's a weekend!"); 
+} 
+else if (num==5)
+{
+    Console.WriteLine("TGIF!");   
+}
+else Console.WriteLine("This day of the week doesn't exist. Try again!");  
