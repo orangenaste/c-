166b86fe877ed519938b6d8a060c2c35fff7d2a4
orangenaste <orangenaste@gmail.com>
2022-12-15 11:43:28 +0300

    Add Task 13 solution

--- Task13/Program.cs
+++ Task13/Program.cs
@@ -3,3 +3,23 @@
 // 645 -> 5
 // 78 -> третьей цифры нет
 // 32679 -> 6
+
+int 
+static void ViewaThirdDigit2(int num, int index = 3);
+{
+Console.Clear();
+
+// if (num-100<0)
+if (num < 100)
+{
+    Console.WriteLine("The third digit doesn't exist");
+   return;
+}
+while (num > 100)
+{ num /= 10;
+    //int thirdDigit = (num/100)%10;
+    // int firstDigit = num / 100;
+    // int secondDigit = (num/10)%10;
+    // int thirdDigit = num%10;
+Console.WriteLine($"Third digit of {num} is {thirdDigit}");
+}}
