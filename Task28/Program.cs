16d57109f074e31347a7c567897a15a6d37e6778
orangenaste <orangenaste@gmail.com>
2022-12-26 19:38:31 +0300

    Add solution Task 28

---
+++ Task28/Program.cs
@@ -0,0 +1,16 @@
+﻿// // Задача 28: Напишите программу, которая
+// принимает на вход число N и выдаёт
+// произведение чисел от 1 до N.
+// 4 -> 24
+// 5 -> 120
+
+Console.WriteLine("Enter num: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int a = 1;
+
+for (int i=1; i<=num; i++)
+{
+ a = a*i;
+}
+Console.WriteLine($"{a}");
