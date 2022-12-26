eebafa1f252843d99f9690f149239808f8e7fd7f
orangenaste <orangenaste@gmail.com>
2022-12-26 17:41:24 +0300

    Add solution Task 26

---
+++ Task26/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 26: Напишите программу, которая принимает
+// на вход число и выдаёт количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+Console.WriteLine("Enter num: ");
+int num = int.Parse(Console.ReadLine());
+int i = 0;
+
+for (;num > 0; num/=10)
+{
+ i++;
+}
+Console.WriteLine($"there are {i} digits in this number");
