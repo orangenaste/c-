9ef94b94c1337c67bc4ba3c821b1147a3d2d6495
orangenaste <orangenaste@gmail.com>
2022-12-26 23:22:08 +0300

    Add Task 29 solution

---
+++ Task29/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 29: Напишите программу, которая задаёт
+// массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+// пользователю нужно ввести самому цифры
+
+int[] array = new int[8];
+
+for (int i =0; i < array.Length; i++)
+{
+    Console.Write($"Enter the array elements under the index {i}  ");
+    array[i] = int.Parse(Console.ReadLine());
+}
+Console.Write("Your array is ");
+Console.WriteLine(string.Join(", ", array));
