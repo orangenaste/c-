d38e9e71b84cf53d79bdac0eb80df2e1ae89e755
orangenaste <orangenaste@gmail.com>
2022-12-29 08:36:45 +0300

    Add alternative solution

--- Task29/Program.cs
+++ Task29/Program.cs
@@ -13,3 +13,17 @@ for (int i =0; i < array.Length; i++)
 }
 Console.Write("Your array is ");
 Console.WriteLine(string.Join(", ", array));
+
+// Решение с семинара:
+// int [] array = GetRandomArray(8, 0, 100);
+// Console.WriteLine($"[{String.Join(",", array)}]");
+
+// int[] GetRandomArray(int size, int minValue, int maxValue)
+// {
+//     int[] result = new int[size];
+//     for (int i = 0; i < size; i++)
+//     {
+//         result[i] = new Random().Next(minValue, maxValue + 1);
+//     }
+//     return result;
+// }
