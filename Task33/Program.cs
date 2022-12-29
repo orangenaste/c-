5c4095d120fad1ccbbb22c7c21e407276f2b24c0
orangenaste <orangenaste@gmail.com>
2022-12-29 09:11:13 +0300

    Add Task 33 solution from Seminar 5

---
+++ Task33/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 33: Задайте массив. Напишите программу, которая
+// определяет, присутствует ли заданное число в массиве.
+// 4; массив [6, 7, 19, 345, 3] -> нет
+// -3; массив [6, 7, 19, 345, 3] -> да
+
+Console.WriteLine("Enter num: ");
+int num = int.Parse(Console.ReadLine());
+int[] array = {6, 7, 19, 345, 3};
+
+bool wr = false;
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] == num)
+    {
+        wr = true;
+    }
+}
+
+string str = wr ? "Yes" : "No";
+Console.WriteLine(str);
