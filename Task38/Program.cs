9eb19b3b7f0da44529a3b537e5764e488f4aa490
orangenaste <orangenaste@gmail.com>
2022-12-29 11:07:54 +0300

    Add solution Task38

---
+++ Task38/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 38: Задайте массив вещественных чисел. 
+// Найдите разницу между максимальным и 
+// минимальным элементов массива.
+// [3 7 22 2 78] -> 76
+
+int[] array = { 3, 7, 22, 2, 78 };
+Console.WriteLine($"[{String.Join(",", array)}]");
+
+int maxelementarray = array[0];
+int minelementarray = array[0];
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] > maxelementarray)
+    {
+        maxelementarray = array[i];
+    }
+    else if (array[i] < minelementarray)
+    {
+        minelementarray = array[i];
+    }
+}
+int differencemaxmin = maxelementarray - minelementarray;
+Console.WriteLine($"Difference between max element {maxelementarray} and min element {minelementarray} is {differencemaxmin}");
