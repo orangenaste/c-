18e09b30caada92b6ff04bc289744bdbeb36b7c9
orangenaste <orangenaste@gmail.com>
2023-01-02 17:45:17 +0300

    Add solution Task 52

---
+++ Task52/Program.cs
@@ -0,0 +1,56 @@
+﻿// Задача 52. Задайте двумерный массив из целых чисел. 
+// Найдите среднее арифметическое элементов в каждом столбце.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+int rows = 3;
+int columns = 4;
+int[,] array = GetArray(rows, columns, 0, 10);
+PrintArray(array);
+
+int[,] GetArray(int rows, int columns, int minValue, int maxValue)
+{
+    int[,] result = new int[rows, columns];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }
+    return result;
+}
+
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+double[] arithmeticMeanColumns = GetResultArray(array);
+
+double[] GetResultArray(int[,] array)
+{
+    double[] arithmeticMeanColumns = new double[array.GetLength(1)];
+    for (int i = 0; i < array.GetLength(1); i++)
+    {
+        double sum = 0;
+        for (int j = 0; j < array.GetLength(0); j++)
+        {
+            sum += array[j, i];
+        }
+        arithmeticMeanColumns[i] = Math.Round(sum / array.GetLength(0), 2);
+    }
+    return arithmeticMeanColumns;
+}
+
+Console.WriteLine($"The arithmetic mean of the elements in each column = {String.Join("; ", arithmeticMeanColumns)}");
