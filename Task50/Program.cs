787fcb42a30186e8c4262acadaac832d15ad76d8
orangenaste <orangenaste@gmail.com>
2023-01-02 15:15:40 +0300

    Add solution Task 50

---
+++ Task50/Program.cs
@@ -0,0 +1,65 @@
+﻿// Задача 50. Напишите программу, которая на вход принимает 
+// позиции элемента в двумерном массиве, и возвращает значение 
+// этого элемента или же указание, что такого элемента нет.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 17 -> такого числа в массиве нет
+
+Console.Write("Input number of array's rows: ");
+var rows = int.Parse(Console.ReadLine());
+Console.Write("Input number of array's columns: ");
+var columns = int.Parse(Console.ReadLine());
+
+int[,] array = GetArray(rows, columns, 0, 10);
+PrintArray(array);
+
+int[,] GetArray(int rows, int columns, int minValue, int maxValue)
+{
+    int[,] result = new int[rows, columns];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }
+    return result;
+}
+
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.Write("Enter the value of an element to check if it is in the array: ");
+int number=int.Parse(Console.ReadLine());
+
+bool FindNumber(int[,] array, int num)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (array[i, j]==num) return true;
+        }
+    }
+    return false;
+}
+
+if(FindNumber(array,number))
+{
+    Console.WriteLine($"{number}");
+}
+else
+{
+    Console.WriteLine("There is no such number in the array");
+}
