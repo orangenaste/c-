5cd9a1d4c4fb68f659f9ccfe4cc90c329c583e94
orangenaste <orangenaste@gmail.com>
2023-01-02 15:26:50 +0300

    Add Task 48 solution from Seminar 7

---
+++ Task48/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 48: Задайте двумерный массив размера m на n,
+// каждый элемент в массиве находится по формуле: Aₙₙ = m+n.
+// Выведите полученный массив на экран.
+// m = 3, n = 4.
+// 0 1 2 3
+// 1 2 3 4
+// 2 3 4 5
+
+Console.Write("Input number of array's rows: ");
+var rows = int.Parse(Console.ReadLine());
+Console.Write("Input number of array's columns: ");
+int columns = int.Parse(Console.ReadLine());
+
+int[,] array = new int[rows, columns];
+
+for (int i = 0; i < rows; i++)
+{
+    for (int j = 0; j < columns; j++)
+    {
+        array[i, j] = i + j;
+    }
+}
+
+for (int i = 0; i < rows; i++)
+{
+    for (int j = 0; j < columns; j++)
+    {
+        Console.Write(array[i, j] + " ");
+    }
+    Console.WriteLine();
+}
